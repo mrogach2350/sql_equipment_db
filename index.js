2a156badb2bfdd4e61305b6d1a5de4cbda59121a
Michael Rogachevsky <michael.rogachevsky@healthland.com>
2018-06-04 15:29:59 -0500

    creates routes and weapon router

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 require('dotenv').config();
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    database: process.env.DATABASE,
-    username: process.env.NAME || 'root',
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
-
+const Sequelize = require('./db').Sequelize;
+const sequelize = require('./db').sequelize;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 const PORT = process.env.PORT || 6969;
 
+//Bring in Each Model
 require('./schema')(Sequelize, sequelize);
 const Armor = require('./schema').Armor;
 const Weapon = require('./schema').Weapon;
@@ -29,56 +15,11 @@ app.get('/', (req, res) => {
     res.send('go to /api for more stuff');
 });
 
-app.get('/api/weapons', (req, res) => {
-    Weapon.findAll()
-    .then(results => {
-        res.json(results);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-});
-
-app.get('/api/weapons/:id', (req, res) => {
-    let weaponId = req.params.id;
-    Weapon.findById(weaponId)
-    .then(result => {
-        res.json(result);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-});
-
-app.post('/api/weapons', (req, res) => {
-    let weapon = Weapon.build(req.body);
-    weapon.save()
-    .then(result => res.json(result))
-    .catch(err => res.json(err));
-});
-
-app.post('/api/weapons/seed', (req, res) => {
-    let mockWeapons = require('./Mock_Weapons.json');
-    Weapon.bulkCreate(mockWeapons)
-    .then(results => {
-        res.json({
-            success: true,
-            message: `${results.length} weapons created`
-        })
-    })
-    .catch(err => res.json(err));
-});
-
-app.delete('/api/weapons/seed', (req, res) => {
-    Weapon.sync({ force: true })
-    .then(result => {
-        res.json({
-            success: true,
-            message: 'Weapons Table cleared'
-        })
-    })
-    .catch(err => res.json(err));
-});
+//Weapon Router + Controller
+const weaponRouter = require('./routes/weapon.router')(express, Weapon);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api', weaponRouter);``
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => {
---
+++ routes/weapon.router.js
@@ -0,0 +1,93 @@
+module.exports = (express, Weapon) => {
+    let weaponRouter = express.Router();
+
+    //LIST 
+    weaponRouter.get('/weapons', (req, res) => {
+        Weapon.findAll()
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    });
+
+    //GET BY ID
+    weaponRouter.get('/weapons/:id', (req, res) => {
+        let weaponId = req.params.id;
+        Weapon.findById(weaponId)
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    });
+
+    //CREATE
+    weaponRouter.post('/weapons', (req, res) => {
+        let weapon = Weapon.build(req.body);
+        weapon.save()
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
+    });
+
+    //UPDATE
+    weaponRouter.put('/weapons/:id', (req, res) => {
+        let weaponId = req.params.id;
+        let weaponData = req.body;
+        Weapon.findById(weaponId)
+        .then(weapon => {
+            weapon.update(weaponData)
+            .then(result => {
+                res.json({
+                    success: true,
+                    data: result
+                });
+            });
+        })
+        .catch(err => res.json(err));
+    });
+
+    //DELETE
+    weaponRouter.delete('/weapons/:id', (req, res) => {
+        let weaponId = req.params.id;
+        Weapon.findById(weaponId)
+        .then(weapon => {
+            weapon.destroy();
+            res.json({
+                success: true,
+                data: weapon,
+                message: 'Successfully deleted'
+            });
+        })
+        .catch(err => res.json(err));
+    });
+
+    //SEED - CREATE
+    weaponRouter.post('/weapons/seed', (req, res) => {
+        let mockWeapons = require('./Mock_Weapons.json');
+        Weapon.bulkCreate(mockWeapons)
+        .then(results => {
+            res.json({
+                success: true,
+                message: `${results.length} weapons created`
+            })
+        })
+        .catch(err => res.json(err));
+    });
+
+    //DELETE - API
+    weaponRouter.delete('/weapons/seed', (req, res) => {
+        Weapon.sync({ force: true })
+        .then(result => {
+            res.json({
+                success: true,
+                message: 'Weapons Table cleared'
+            })
+        })
+        .catch(err => res.json(err));
+    });
+
+    return weaponRouter;
+}
