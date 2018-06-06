e4db996ad2efb140ea6d5b731402c491dde7a8fa
Michael Rogachevsky <michael.rogachevsky@healthland.com>
2018-06-06 07:43:23 -0500

    allows CORS

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ app.get('/', (req, res) => {
 const weaponRouter = require('./routes/weapon.router')(express, Weapon);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/api', weaponRouter);``
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use('/api', weaponRouter);
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => {
